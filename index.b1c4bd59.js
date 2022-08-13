b39d46e0c820540feb9e5c219fae437ff8cd4684
OlegDerevyanko <106957951+OlegDerevyanko@users.noreply.github.com>
2022-08-13 10:05:10 +0000

    Deploying to gh-pages from @ Hrodetskyi/team-project@a979e763b5c3ac089223af291af3d5194f4e0f29 🚀

---
+++ index.b1c4bd59.js
@@ -0,0 +1,2 @@
+function backToTop(){var o=this,n=$(".back-to-top");$(window).on("scroll",(function(){$(o).scrollTop()>=50?n.fadeIn():n.fadeOut()})),n.on("click",(function(){e.preventDefault(),$("html").animate({scrollTop:0},1e3)}))}backToTop();
+//# sourceMappingURL=index.b1c4bd59.js.map
