9b580eaca77f66b96153b0eafe55d55035cf91f7
OlegDerevyanko <106957951+OlegDerevyanko@users.noreply.github.com>
2022-08-17 07:45:20 +0300

    up

---
+++ src/js/hero-modal.js
@@ -0,0 +1,14 @@
+  (() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-hero-open]"),
+    closeModalBtn: document.querySelector("[data-hero-close]"),
+    modal: document.querySelector("[data-hero]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden-hero");
+  }
+})();
