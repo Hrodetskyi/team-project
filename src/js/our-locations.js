beefa4b303396a03cfb1d3f08197bab22197629a
OlegDerevyanko <106957951+OlegDerevyanko@users.noreply.github.com>
2022-08-20 21:10:44 +0300

    up

--- src/js/our-locations.js
+++ src/js/our-locations.js
@@ -9,6 +9,6 @@
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden-hero");
+    refs.modal.classList.toggle("is-hidden-location");
   }
 })();
